f7b3c44d8bf2aa303915dc49309d42e9209d43b9
maryam <maryam@informatik.uni-bremen.de>
2014-12-29 12:46:40 +0100

    contactform

--- website/academic-education/pages/contact.php
+++ website/academic-education/pages/contact.php
@@ -1,193 +1,91 @@
-<!DOCTYPE html>
-<!--
-Template Name: Academic Education V2
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
--->
-<html>
+<?PHP
+require_once("../include/membersite_config.php");
+
+if(isset($_POST['submitted']))
+{
+   if($fgmembersite->RegisterUser())
+   {
+        $fgmembersite->RedirectToURL("./style/thank-you.html");
+   }
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-<title>Limba</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+    <title>Contact us</title>
+    <link rel="STYLESHEET" type="text/css" href="../style/fg_membersite.css" />
+    <script type='text/javascript' src='../scripts/gen_validatorv31.js'></script>
+    <link rel="STYLESHEET" type="text/css" href="../style/pwdwidget.css" />
+    <script src="../scripts/pwdwidget.js" type="text/javascript"></script>      
 </head>
-<body id="top">
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
-<div class="wrapper row0">
-  <div id="topbar" class="clear"> 
-    <!-- ################################################################################################ -->
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">A - Z Index</a></li>
-        <li><a href="../pages/loginform.php">Login</a></li>
-        <li><a href="registerform.php">Register</a></li>
-      </ul>
-    </nav>
-    <!-- ################################################################################################ --> 
-  </div>
-</div>
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
-<div class="wrapper row1">
-  <header id="header" class="clear"> 
-    <!-- ################################################################################################ -->
-    <div id="logo" class="fl_left">
-      <h1><a href="index.html">Limba</a></h1>
-      
-    </div>
-    <div class="fl_right">
-      <form class="clear" method="post" action="#">
-        <fieldset>
-          <legend>Search:</legend>
-          <input type="text" value="" placeholder="Search Here">
-          <button class="fa fa-search" type="submit" title="Search"><em>Search</em></button>
-        </fieldset>
-      </form>
-    </div>
-    <!-- ################################################################################################ --> 
-  </header>
-</div>
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
-<div class="wrapper row2">
-  <div class="rounded">
-    <nav id="mainav" class="clear"> 
-      <!-- ################################################################################################ -->
-      <ul class="clear">
-        <li class="active"><a href="index.html">Home</a></li>
-        <li><a class="drop" href="#">Pages</a>
-          <ul>
-            <li><a href="pages/gallery.html">New words</a></li>
-            <li><a href="pages/portfolio.html">Vocabulary Lists</a></li>
-            <li><a href="pages/full-width.html">Add Words</a></li>
-            <li><a href="pages/sidebar-left.html">Evaluation</a></li>
-           <!--<li><a href="pages/sidebar-left-2.html">Sidebar Left 2</a></li>
-            <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
-            <li><a href="pages/sidebar-right-2.html">Sidebar Right 2</a></li>
-            <li><a href="pages/basic-grid.html">Basic Grid</a></li> -->
-          </ul>
-        </li>
-        <li><a class="drop" href="#">Dropdown</a>
-          <ul>
-            <li><a href="#">Level 2</a></li>
-            <li><a class="drop" href="#">Level 2 + Drop</a>
-              <ul>
-                <li><a href="#">Level 3</a></li>
-                <li><a href="#">Level 3</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="#">Link Text</a></li>
-        <li><a href="#">Another Link Text</a></li>
-        <li><a href="#">This a very long link</a></li>
-        <li><a href="#">This is the last</a></li>
-      </ul>
-      <!-- ################################################################################################ --> 
-    </nav>
-  </div>
-</div>
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
+<body>
 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
+<!-- Form Code Start -->
+<div id='fg_membersite'>
+<form id='register' action='<?php echo $fgmembersite->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Register</legend>
 
-  
-       
-         
-          <!-- ################################################################################################ --> 
-       
-      <!-- ################################################################################################ --> 
-      <!-- ################################################################################################ -->
-      <div id="twitter" class="group btmspace-30">
-        <div class="one_quarter first center"><a href="#"><i class="fa fa-twitter fa-3x"></i><br>
-          Follow Us On Twitter</a></div>
-        <div class="three_quarter bold">
-          <p>Inteligula congue id elis donec sce sagittis intes id laoreet aenean. Massawisi condisse leo sem ac tincidunt nibh quis dui fauctor et donecnibh elis velit <a href="#">@name</a> - 10:15 AM yesterday</p>
-        </div>
-      </div>
-      <!-- ################################################################################################ --> 
-      <!-- ################################################################################################ -->
-      
-      
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<div class='short_explanation'>* required fields</div>
+<input type='text'  class='spmhidip' name='<?php echo $fgmembersite->GetSpamTrapInputName(); ?>' />
+
+<div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
+<div class='container'>
+    <label for='name' >Your Full Name*: </label><br/>
+    <input type='text' name='name' id='name' value='<?php echo $fgmembersite->SafeDisplay('name') ?>' maxlength="50" /><br/>
+    <span id='register_name_errorloc' class='error'></span>
+</div>
+<div class='container'>
+    <label for='email' >Email Address*:</label><br/>
+    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
+    <span id='register_email_errorloc' class='error'></span>
+</div>
+<div class='container' style='height:-3px;'>
+    <label for='password' >Your message*:</label><br/>
    
-       
-      <!-- ################################################################################################ --> 
-      <!-- / main body -->
-      <div class="clear"></div>
-    </main>
-  </div>
+    
+    <textarea name='message' rows = "7" cols= "30"></textarea> <br/>
+      
+    <div id='register_password_errorloc' class='error' style='clear:both'></div>
 </div>
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
-<div class="wrapper row4">
-  <div class="rounded">
-    <footer id="footer" class="clear"> 
-      <!-- ################################################################################################ -->
-      <div class="one_third first">
-        <figure class="center"><img class="btmspace-15" src="../images/demo/worldmap.png" alt="">
-          <figcaption><a href="#">Find Us With Google Maps &raquo;</a></figcaption>
-        </figure>
-      </div>
-      <div class="one_third">
-        <address>
-        Long Educational Facility Name<br>
-        Address Line 2<br>
-        Town/City<br>
-        Postcode/Zip<br>
-        <br>
-        <i class="fa fa-phone pright-10"></i> xxxx xxxx xxxxxx<br>
-        <i class="fa fa-envelope-o pright-10"></i> <a href="#">contact@domain.com</a>
-        </address>
-      </div>
-      <div class="one_third">
-        <p class="nospace btmspace-10">Stay Up to Date With What's Happening</p>
-        <ul class="faico clear">
-          <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-          <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a class="faicon-flickr" href="#"><i class="fa fa-flickr"></i></a></li>
-          <li><a class="faicon-rss" href="#"><i class="fa fa-rss"></i></a></li>
-        </ul>
-        <form class="clear" method="post" action="#">
-          <fieldset>
-            <legend>Subscribe To Our Newsletter:</legend>
-            <input type="text" value="" placeholder="Enter Email Here&hellip;">
-            <button class="fa fa-sign-in" type="submit" title="Sign Up"><em>Sign Up</em></button>
-          </fieldset>
-        </form>
-      </div>
-      <!-- ################################################################################################ --> 
-    </footer>
-  </div>
+
+<div class='container'>
+    <input type='submit' name='Send Email' value='Send Email' />
 </div>
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ --> 
-<!-- ################################################################################################ -->
-<div class="wrapper row5">
-  <div id="copyright" class="clear"> 
-    <!-- ################################################################################################ -->
-    <p class="fl_left">Copyright &copy; 2014 - All Rights Reserved - <a href="#">Domain Name</a></p>
-    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
-    <!-- ################################################################################################ --> 
-  </div>
-</div>
-<!-- JAVASCRIPTS --> 
-<script src="../layout/scripts/jquery.min.js"></script> 
-<script src="../layout/scripts/jquery.fitvids.min.js"></script> 
-<script src="../layout/scripts/jquery.mobilemenu.js"></script> 
-<script src="../layout/scripts/tabslet/jquery.tabslet.min.js"></script>
+
+</fieldset>
+</form>
+<!-- client-side Form Validations:
+Uses the excellent form validation script from JavaScript-coder.com-->
+
+<script type='text/javascript'>
+// <![CDATA[
+    var pwdwidget = new PasswordWidget('thepwddiv','password');
+    pwdwidget.MakePWDWidget();
+    
+    var frmvalidator  = new Validator("register");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("username","req","Please provide a username");
+    
+    frmvalidator.addValidation("password","req","Please provide a password");
+
+// ]]>
+</script>
+
+<!--
+Form Code End (see html-form-guide.com for more info.)
+-->
+
 </body>
 </html>
